ec64e27f37f8c4bf88c2c7f278e79d12a0786b8f
mzaguilar8 <mario.zepeda@upr.edu>
2016-11-14 03:03:01 -0400

    Update gisclass.py
    Ademas de las que estaban planeadas agregue la clase Point y la Clase Line.

--- gisclass.py
+++ gisclass.py
@@ -107,3 +107,140 @@ class Street(object):
             return iter(self.edifications[key]);
         else:
             return iter([]);
+
+#Agregue la clase Point y la Clase Line
+
+class Point(object):
+
+    def __init__(self, x, y,label = "NONAME"):
+        self.X = x
+        self.Y = y
+        self.L = label
+
+    def __str__(self):
+        return "Point(%s,%s,%s)"%(self.X, self.Y,self.L)
+
+    def move(self, dx, dy):
+        self.X = self.X + dx
+        self.Y = self.Y + dy
+
+    def Distance(self, other):
+        dx = self.X - other.X
+        dy = self.Y - other.Y
+        return math.sqrt(dx**2 + dy**2)
+
+class Region(object):
+
+    def __init__(self, puntos, label = "NoName"):
+        self.Puntos = puntos
+        self.label = label
+        self.n = len(puntos)
+
+    def __add__(self, other):
+        self.Puntos.append(other)
+
+    def Print(self):
+        for punto in self.Puntos:
+            print (punto.X,punto.Y)
+
+    def Perimeter(self):
+        perimeter = 0
+        for x in range(-1,self.n-1):
+            perimeter = perimeter + self.Puntos[x].Distance(self.Puntos[x+1])
+        print perimeter
+        return perimeter
+
+
+
+class Line(object):
+
+    def __init__(self, puntos, label = "NoName"):
+        self.Puntos = puntos
+        self.label = label
+
+    def __add__(self, other):
+        self.Puntos.append(other)
+
+    def Print(self):
+        for x in self.Puntos:
+            print x
+
+class Block(Edification,Region):
+
+    def __init__(self,edificaciones, limites, label = "NONAME"):
+        self.Viviendas = edificaciones
+        self.Limites = Region(limites)
+        self.Label = label
+
+    def __add__(self, other):
+        self.Viviendas.append(other)
+
+    def Print(self):
+        for edi in self.Viviendas:
+            edi.Print()
+        Limites.Print()
+
+class Neighborhoood(Block,Region):
+
+    def __init__(self, bloques, limites, label = "NONAME"):
+        self.Bloques = bloques
+        self.Limites = Region(limites)
+        self.Label = label
+
+    def __add__(self, other):
+        self.Bloques.append(other)
+
+
+
+class Town(Neighborhoood,Region):
+
+    def __init__(self, vecindarios, limites, label = "NONAME"):
+        self.Vecindarios = vecindarios
+        self.Limites = Region(limites)
+        self.Label = label
+
+    def __add__(self, other):
+        self.Vecindarios.append(other)
+
+
+class State(Town,Region):
+
+    def __init__(self, ciudades, limites, label = "NONAME"):
+        self.Ciudadades = ciudades
+        self.Limites = Region(limites)
+        self.Label = label
+
+    def __add__(self, other):
+        self.Ciudadades.append(other)
+
+
+class Lake(object):
+
+    def __init__(self, puntos, label = "NoName"):
+        self.Puntos = puntos
+        self.label = label
+        self.n = len(puntos)
+
+    def __add__(self, other):
+        self.Puntos.append(other)
+
+    def Print(self):
+        for punto in self.Puntos:
+            print (punto.X,punto.Y)
+
+    def Perimeter(self):
+        perimeter = 0
+        for x in range(-1,self.n-1):
+            perimeter = perimeter + self.Puntos[x].Distance(self.Puntos[x+1])
+        print perimeter
+        return perimeter
+
+class River(Line):
+
+    def __init__(self, puntosA, puntosB, label = "NoName"):
+        self.ladoA = Line(puntosA)
+        self.ladoB = Line(puntosB)
+        self.label = label
+        self.n = len(puntosA)
+        
+        
